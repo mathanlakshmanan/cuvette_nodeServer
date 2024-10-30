a4582b348fd7d928b91e51f5496042792b407c8b
mathanlakshmanan <mathankumarmca92@gmail.com>
2024-10-30 21:12:54 +0530

    body updated

--- controller/userController.js
+++ controller/userController.js
@@ -114,14 +114,14 @@ const mobileVerifyController = async(req, res)=>{
     const id = req.params.id;
     const dbData = await User.findById({_id:id},{mobileOTP:1, mobileVerify:1});
     try {
-        if(dbData.mobileVerify === false){
-        if(dbData.mobileOTP === body.mobileotp){
+        if(dbData.mobileVerify == false){
+        if(dbData.mobileOTP == body.mobileotp){
             const updateMobileVerify = await User.findByIdAndUpdate({_id:id}, {mobileVerify:true}, {new:true});
             res.status(200).json({data:updateMobileVerify, message:"Mobile OTP Verification is Done"});
     }else{
         res.status(304).json({data:"", message:"Mobile OTP Number is Invalid"});
     }}else{
-        if(dbData.mobileOTP === data.mobileotp){
+        if(dbData.mobileOTP == body.mobileotp){
             res.status(304).json({data:"", message:"Mobile OTP Number Already verified"});
         }else{
             res.status(304).json({data:"", message:"Mobile OTP Number Already verified And Provide Mobile OTP Number is Invalid"});
@@ -139,14 +139,14 @@ const emailVerifyController = async(req, res)=>{
     
     const dbData = await User.findById({_id:id},{emailOTP:1, emailverify:1});
     try {
-        if(dbData.emailverify === false){
-        if(dbData.emailOTP === body.emailotp){
+        if(dbData.emailverify == false){
+        if(dbData.emailOTP == body.emailotp){
             const updateEmailVerify = await User.findByIdAndUpdate({_id:id}, {emailverify:true}, {new:true});
             res.status(200).json({data:updateEmailVerify, message:"Email OTP Verification is Done"});
     }else{
         res.status(304).json({data:"", message:"Email OTP Number is Invalid"});
     }}else{
-        if(dbData.emailOTP === data.emailotp){
+        if(dbData.emailOTP === body.emailotp){
             res.status(304).json({data:"", message:"Email OTP Number Already verified"});
         }else{
             res.status(304).json({data:"", message:"Email OTP Number Already verified And Provide Email OTP Number is Invalid"});
